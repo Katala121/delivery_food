5d439dadc507fa9398fa66ffd5aab0c9c845528e
Katala121 <sanyafootb@list.ru>
2021-02-14 23:40:02 +0300

    added auth tests

---
+++ __test__/middleware/auth.spec.js
@@ -0,0 +1,96 @@
+import express        from 'express';
+import request        from 'supertest';
+import User           from '../../src/models/User.js';
+import Admin           from '../../src/models/Admin.js';
+import UserRouter     from '../../src/routers/UserRouter.js';
+import AdminRouter     from '../../src/routers/AdminRouter.js';
+import UserRepository from '../../src/repositories/UserRepository.js';
+import AdminRepository from '../../src/repositories/AdminRepository.js';
+import auth           from '../../src/middleware/Auth.js';
+
+const app = express();
+
+const client = {
+    query: jest.fn(),
+    release: jest.fn()
+};
+const pool = {
+    connect: jest.fn(() => client),
+    query: jest.fn()
+};
+
+jest.mock('../../src/repositories/UserRepository.js');
+jest.mock('../../src/repositories/AdminRepository.js');
+
+describe('test auth route', () => {
+    test('test authUser method success answer', async () => {
+        const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im15YWRyZXNzQGdtYWlsLmNvbSIsIm5hbWUiOiJBbGV4IiwiaWQiOiIxIiwiaWF0IjoxNjEzMzMxOTg2LCJleHAiOjE2MTM0MTgzODZ9.2YY0t0Ro6ld1A1F4E8oauZ8Mpi95opUlu_bWbZunUaI';
+        
+        const user = new User({
+            id: '1',
+            name: 'Alex',
+            email: 'myadress@gmail.com',
+        });
+        user._password = '$2a$15$3mzFsJ4wV7rBuChzRPRDbOaqPXasB0ugaIM1AiCW8py0EwWymQq4S';
+        
+        UserRepository.mockImplementation(() => ({
+                findByEmailAndId: () => user
+        }));
+        const userRouter = new UserRouter(pool);
+        const res = await request(app.use('/api/users', userRouter.router))
+            .get('/api/users/1')
+            .set('Authorization', token);
+
+        const response = res.body;
+
+        expect(JSON.stringify(response))
+            .toBe(JSON.stringify(user));
+    });
+
+    test('test authUser method ERROR answer', async () => {
+                
+        const userRouter = new UserRouter(pool);
+        const res = await request(app.use('/api/users', userRouter.router))
+            .get('/api/users/1');
+
+        const response = res.text.indexOf('Invalid auth data');
+
+        expect(response);
+    });
+
+    test('test authAdmin method success answer', async () => {
+        const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXN0YXVyYW50X2lkIjoxLCJuYW1lIjoic3VzaGkgbWFzdGVyIGFkbWluLXlvbGEiLCJpZCI6MSwiaWF0IjoxNjEzMzM0MzAyLCJleHAiOjE2MTM0MjA3MDJ9.UFq0yaqat_DftaZ8MOPaHR1U1TedaHwapJecnRB-lXc';
+        
+        const admin = new Admin({
+            id: 1,
+            name: 'Alex',
+            restaurant: 1,
+        });
+        admin._password = '$2a$15$fLlELgwhSEpDrjd8FgH7Fu7t0YGbpJp6Az7.qs6uDUC0I94iwPXBq';
+        
+        AdminRepository.mockImplementation(() => ({
+            findByNameAdminAndRestaurantId: () => admin,
+            get: () => admin,
+        }));
+        const adminRouter = new AdminRouter(pool);
+        const res = await request(app.use('/api/admins', adminRouter.router))
+            .get('/api/admins/1')
+            .set('Authorization', token);
+
+        const response = res.body;
+
+        expect(JSON.stringify(response))
+            .toBe(JSON.stringify(admin));
+    });
+
+    test('test authAdmin method ERROR answer', async () => {
+                
+        const adminRouter = new AdminRouter(pool);
+        const res = await request(app.use('/api/admins', adminRouter.router))
+            .get('/api/admins/1');
+
+        const response = res.text.indexOf('Invalid auth data');
+
+        expect(response);
+    });
+});
